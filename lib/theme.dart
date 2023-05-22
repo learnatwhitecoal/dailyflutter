36df4d27b1cbb9f04f1fd62ad9c204cdb9445743
Sundar Gautam <sundargautam@cagtu.com>
2023-05-22 19:38:08 +0545

    added files

---
+++ lib/theme.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+ThemeData theme() {
+  return ThemeData(
+    scaffoldBackgroundColor: Colors.white,
+    fontFamily: "Muli",
+    appBarTheme: appBarTheme(),
+    visualDensity: VisualDensity.adaptivePlatformDensity,
+  );
+}
+
+AppBarTheme appBarTheme() {
+  return const AppBarTheme(backgroundColor: Colors.amber);
+}
